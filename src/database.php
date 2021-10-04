77be57d4bca4331594703848da22b724e36f7753
Ager Hilson <ahilson@students.kennesaw.edu>
2021-10-04 10:39:31 -0400

    second

---
+++ src/database.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "mysqlserverksu123.database.windows.net";
+$username = "azureuser";
+$password = "sample!23";
+$dbname = "mySampleDatabse";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$conn->close();
+?>
